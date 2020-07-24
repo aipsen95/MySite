1e8f6455017a927b050f7924fd3e46f00437f21a
Anders Ravn Ipsen <aipsen95@gmail.com>
2020-07-24 12:40:30 +0200

    Updates

---
+++ precache-manifest.2cf17c306fcbe34034f898de4f27ad72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2341feeb8f657412328203fd0372d183",
+    "url": "/MySite/index.html"
+  },
+  {
+    "revision": "8bc71d5e39543bfc716e",
+    "url": "/MySite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/MySite/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MySite/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc71d5e39543bfc716e",
+    "url": "/MySite/static/js/main.40672370.chunk.js"
+  },
+  {
+    "revision": "2f8467601d959739df0a",
+    "url": "/MySite/static/js/runtime-main.44788157.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MySite/static/media/logo.5d5d9eef.svg"
+  }
+]);
